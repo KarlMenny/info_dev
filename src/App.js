5df3de940331a8055704a83ac3a90d42be46bf90
Andreii <andreii.kirichenko@gmail.com>
2023-07-23 14:05:26 +0300

    lazy added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import About from './pages/About/About';
-import GalleryPhotos from './pages/Gallery/GalleryPhotos';
-import Сontacts from './pages/Сontacts';
-import Messages from './pages/Messages';
 import TopNavigation from './components/TopNavigation';
+import About from './pages/About/About';
+const GalleryPhotos = lazy(() => import('./pages/Gallery/GalleryPhotos'));
+const Contacts = lazy(() => import('./pages/Сontacts'));
+const Messages = lazy(() => import('./pages/Messages'));
 
 function App() {
   return (
     <BrowserRouter>
-      <Container>
-        <Row>
-          <Col>
-            <TopNavigation />
-            <main>
-              <Routes>
-                <Route path="/" element={<About />} />
-                <Route path="/gallery" element={<GalleryPhotos />} />
-                <Route path="/contacts" element={<Сontacts />} />
-                <Route path="/contacts/:id" element={<Сontacts />} />
-                <Route path="/messages" element={<Messages />} />
-                <Route path="/messages/:id" element={<Messages />} />
-              </Routes>
-            </main>
-          </Col>
-        </Row>
-      </Container>
+      <Suspense fallback={<div className="spinner">LOADING...</div>}>
+        <Container>
+          <Row>
+            <Col>
+              <TopNavigation />
+              <main>
+                <Routes>
+                  <Route path="/" element={<About />} />
+                  <Route path="/gallery" element={<GalleryPhotos />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/contacts/:id" element={<Contacts />} />
+                  <Route path="/messages" element={<Messages />} />
+                  <Route path="/messages/:id" element={<Messages />} />
+                </Routes>
+              </main>
+            </Col>
+          </Row>
+        </Container>
+      </Suspense>
     </BrowserRouter>
   );
 }
